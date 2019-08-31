3565a2837b6564a9fe2bc640b0487a9e8c906871
Jhoy1992 <jhonatanascari@gmail.com>
2019-08-31 15:08:25 -0300

    Few css corrections in Facebook React.

--- desafio04/src/components/Header/Header.js
+++ desafio04/src/components/Header/Header.js
@@ -8,7 +8,7 @@ class Header extends Component {
   render() {
     return (
       <div className="header">
-        <img className="logo" src={facebookLogo} alt="" />
+        <img src={facebookLogo} alt="" />
         <div className="profile">
           <h1>Meu perfil</h1>
           <img src={accountCircle} alt="Account profile" />
